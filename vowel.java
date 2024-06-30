d91d00bbdd525017a7630b1d5c9b6c5762467f79
crarun9740 <crarun9740@gmail.com>
2024-06-30 23:33:48 +0530

    first commit

--- vowel.java
+++ vowel.java
@@ -1,15 +1,16 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class vowel {
      public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string: ");
         String str=sc.nextLine();
         int count=0;
-        for(int i=0;i<=str.length();i++){
+        for(int i=0;i<str.length();i++){
             if(str.charAt(i)=='a' || str.charAt(i)=='e'|| str.charAt(i)=='i'||str.charAt(i)=='o' ||str.charAt(i)=='u'){
                 count++;
             }
         }
-        System.out.println(count);
+        System.out.println("The no. of vowel in the string " + count);
     }
 }
